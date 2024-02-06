3e8605b6fea3451ef554e4333c312931d31b246e
BrunoVettori <bruno_vettori@proton.me>
2024-02-06 19:43:04 -0300

    Init

---
+++ cmd/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"honorarios/internal"
+)
+
+func main() {
+	fmt.Println(internal.Add(4, 78))
+	fmt.Println("Hello World")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module honorarios
+
+go 1.21.6
