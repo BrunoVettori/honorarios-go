a597baab72c519c380c4939fc59922ff003471eb
BrunoVettori <koikke21@gmail.com>
2024-03-05 10:53:57 -0300

    removed null ls

--- cmd/main.go
+++ cmd/main.go
@@ -2,10 +2,22 @@ package main
 
 import (
 	"fmt"
-	"honorarios/internal"
+	"net/http"
 )
 
 func main() {
-	fmt.Println(internal.Add(4, 78))
-	fmt.Println("Hello World")
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Println("Hello World")
+		writer.Write([]byte("Usuário criado"))
+	})
+
+	fmt.Println("Listening on address 8080")
+
+	http.ListenAndServe("localhost:8080", mux)
+
+	// fmt.Println(internal.Add(4, 78))
+	// fmt.Println("Hello World")
 }
